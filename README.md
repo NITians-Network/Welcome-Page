621eb2bf8195f1f0dad507f820e035edb4ace0f8
Prashant Milan Katiyar <55805447+prashantkatiyar9777@users.noreply.github.com>
2020-11-07 21:05:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Welcome-Page
+# WelcomePage
+
+Before building the whole website we are making an animated welcome page for the project.
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/075ba51d-39ec-46b5-ac5a-937e2e0ea47d/deploy-status)](https://app.netlify.com/sites/welcome-page-project-comet/deploys)
---
+++ js/script.js
@@ -0,0 +1,15 @@
+var txt1 = "If you want to go quickly, go alone.";
+var txt2 = "If you want to go far, go togather.";
+var speed = 90; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter(text, id, i = 0) {
+	if (i < text.length) {
+		document.getElementById(id).innerHTML += text.charAt(i);
+		i++;
+		setTimeout(() => typeWriter(text, id, i), speed);
+	}
+}
+
+setTimeout(() => typeWriter(txt1, "subtext1"), 1000);
+
+setTimeout(() => typeWriter(txt2, "subtext2"), 1500 + speed * txt1.length);
